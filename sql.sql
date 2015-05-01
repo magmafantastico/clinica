8d6100e3276c987846dbcd785ee6498f1dd5c7f4
DRJunior <dirceu.r.junior@hotmail.com>
2015-04-30 22:21:52 -0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,4 +1,4 @@
-insert into usuario (ativo, email, login, senha) values (TRUE, dudu@hotmail.com, admin, admin);
+insert into usuario (codigo, ativo, email, login, senha) values (1, TRUE, dudu@hotmail.com, admin, admin);
 
 insert into usuario_permissao values (1, 'ROLE_ADMIN');
 insert into usuario_permissao values (1, 'ROLE_USER');
