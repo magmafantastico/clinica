7623791b64b047096c0e07c2420aa069f95c607d
DRJunior <dirceu.r.junior@hotmail.com>
2015-04-30 22:18:35 -0300

    Create sql.sql

---
+++ sql.sql
@@ -0,0 +1,4 @@
+insert into usuario (ativo, email, login, senha) values (TRUE, dudu@hotmail.com, admin, admin);
+
+insert into usuario_permissao values (1, 'ROLE_ADMIN');
+insert into usuario_permissao values (1, 'ROLE_USER');
